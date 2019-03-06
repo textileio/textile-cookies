37136caed4b5ad929900c53a1e638d3f72f5baa7
Carson Farmer <carson.farmer@gmail.com>
2019-03-06 15:12:14 -0800

    oops missing cookiecutter prefix

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,9 +1,19 @@
 import subprocess
 
 subprocess.call(['git', 'init'])
-subprocess.call(
-    ['curl', '-sL', 'https://www.gitignore.io/api/node', '>', '.gitignore'])
+subprocess.call([
+    'curl', '-sL', 'https://www.gitignore.io/api/node', '>', '.gitignore'
+])
 subprocess.call(['git', 'add', '*'])
-subprocess.call(['git', 'commit', '-m', 'Initial commit'])
+subprocess.call([
+    'git', 'commit', '-m', 'Initial commit'
+])
 subprocess.call(
-    ['git', 'remote', 'add', 'origin', 'https://github.com/{{github_username}}/{{appname}}.git'])
+    [
+        'git',
+        'remote',
+        'add',
+        'origin',
+        'https://github.com/{{cookiecutter.github_username}}/{{cookiecutter.appname}}.git'
+    ]
+)
