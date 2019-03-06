f320b55b2968e1165d7dc00f17efad17ee7ea26b
Carson Farmer <carson.farmer@gmail.com>
2019-03-06 15:09:15 -0800

    more git configuration stuff

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,5 +1,9 @@
 import subprocess
 
 subprocess.call(['git', 'init'])
+subprocess.call(
+    ['curl', '-sL', 'https://www.gitignore.io/api/node', '>', '.gitignore'])
 subprocess.call(['git', 'add', '*'])
 subprocess.call(['git', 'commit', '-m', 'Initial commit'])
+subprocess.call(
+    ['git', 'remote', 'add', 'origin', 'https://github.com/{{github_username}}/{{appname}}.git'])
